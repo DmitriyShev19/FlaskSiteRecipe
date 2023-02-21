b57cfbd604431af300a416710b760318c92ec66e
dimka2338 <dimka2338@ya.ru>
2023-02-21 18:06:44 +0300

    Добавил документацию

--- bisnes_logic.py
+++ bisnes_logic.py
@@ -4,7 +4,19 @@ from app import ALLOWED_EXTENSIONS
 from models import Users
 
 
-def check_new_user(login, email, password):
+def check_new_user(login: str, email: str, password: str) -> bool:
+    """
+        Проверяет данные нового пользователя перед регистрацией.
+
+        :param login: логин пользователя
+        :type login: str
+        :param email: электронная почта пользователя
+        :type email: str
+        :param password: пароль пользователя
+        :type password: str
+        :return: результат проверки
+        :rtype: bool
+    """
     pattern_login = r'^[a-zA-Z!?\d]{5,25}$'
     pattern_email = r'^[-\w\.]+@([-\w]+\.)+[-\w]{2,4}$'
     pattern_password = r'^[a-zA-Z!?\d]{5,36}$'
@@ -38,8 +50,16 @@ def check_new_user(login, email, password):
         return True
 
 
-def allowed_file(filename):
-    """ Функция проверки расширения файла """
+def allowed_file(filename: str) -> bool:
+    """
+        Проверяет расширение загружаемого файла и сравнивает его со списком допустимых
+        расширений, которые хранятся в константе ALLOWED_EXTENSIONS.
+
+        :param filename: имя файла
+        :type filename: str
+        :return: результат проверки
+        :rtype: bool
+    """
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
