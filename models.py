d6b3a0f129b596386f791e990cde7c331e9131aa
dimka2338 <dimka2338@ya.ru>
2023-02-21 23:15:53 +0300

    Использовал библиотеку black поменял двойные кавычки на одинарные, убрал id и created_at в родительский класс BaseModel

--- models.py
+++ models.py
@@ -4,25 +4,28 @@ from app import db, app, manager
 
 
 class BaseModel:
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
+
     def save(self) -> None:
         """
-            Сохраняет текущий экземпляр класса в базе данных
+        Сохраняет текущий экземпляр класса в базе данных
 
-            :return: None
+        :return: None
         """
         db.session.add(self)
         db.session.commit()
 
     @classmethod
-    def create(cls, *args, **kwargs) -> None:
+    def create(cls, *args, **kwargs) -> 'BaseModel':
         """
-            Позволяет создать новый объект в базе данных.
+        Позволяет создать новый объект в базе данных.
 
-            :param args: Позиционные аргументы для передачи конструктору модели
-            :type args: tuple
-            :param kwargs: Аргументы ключевого слова для передачи конструктору модели.
-            :type kwargs: dict
-            :return: None
+        :param args: Позиционные аргументы для передачи конструктору модели
+        :type args: tuple
+        :param kwargs: Аргументы ключевого слова для передачи конструктору модели.
+        :type kwargs: dict
+        :return: None
         """
         new_user = cls(*args, **kwargs)
         new_user.save()
@@ -30,50 +33,49 @@ class BaseModel:
 
 class Users(db.Model, BaseModel, UserMixin):
     """
-        Модель пользователя.
+    Модель пользователя.
 
-        :param id: уникальный идентификатор пользователя
-        :type id: int
-        :param login: логин пользователя
-        :type login: str
-        :param email: адрес электронной почты пользователя
-        :type email: str
-        :param password: пароль пользователя
-        :type password: str
-        :param created_at: дата и время создания записи о пользователе
-        :type created_at: datetime
+    :param id: уникальный идентификатор пользователя
+    :type id: int
+    :param login: логин пользователя
+    :type login: str
+    :param email: адрес электронной почты пользователя
+    :type email: str
+    :param password: пароль пользователя
+    :type password: str
+    :param created_at: дата и время создания записи о пользователе
+    :type created_at: datetime
     """
-    id = db.Column(db.Integer, primary_key=True)
+
     login = db.Column(db.String(64), unique=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
 
 class Recipe(db.Model, BaseModel):
     """
-        Модель рецепта.
+    Модель рецепта.
 
-        :param id: уникальный идентификатор рецепта
-        :type id: int
-        :param id_user: уникальный идентификатор пользователя, создавшего рецепт
-        :type id_user: int
-        :param dish_name: название блюда
-        :type dish_name: str
-        :param food_category: категория блюда
-        :type food_category: str
-        :param file_path: путь к файлу с изображением блюда
-        :type file_path: str
-        :param cooking_time: время приготовления блюда
-        :type cooking_time: str
-        :param ingredients: ингредиенты блюда
-        :type ingredients: str
-        :param recipe: инструкции по приготовлению блюда
-        :type recipe: str
-        :param created_at: дата создания рецепта
-        :type created_at: datetime.datetime
+    :param id: уникальный идентификатор рецепта
+    :type id: int
+    :param id_user: уникальный идентификатор пользователя, создавшего рецепт
+    :type id_user: int
+    :param dish_name: название блюда
+    :type dish_name: str
+    :param food_category: категория блюда
+    :type food_category: str
+    :param file_path: путь к файлу с изображением блюда
+    :type file_path: str
+    :param cooking_time: время приготовления блюда
+    :type cooking_time: str
+    :param ingredients: ингредиенты блюда
+    :type ingredients: str
+    :param recipe: инструкции по приготовлению блюда
+    :type recipe: str
+    :param created_at: дата создания рецепта
+    :type created_at: datetime.datetime
     """
-    id = db.Column(db.Integer, primary_key=True)
+
     id_user = db.Column(db.Integer, db.ForeignKey('users.id'))
     dish_name = db.Column(db.String(100))
     food_category = db.Column(db.String(100))
@@ -81,22 +83,20 @@ class Recipe(db.Model, BaseModel):
     cooking_time = db.Column(db.String(100))
     ingredients = db.Column(db.String(700))
     recipe = db.Column(db.String(1000))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
 
 @manager.user_loader
 def load_user(user_id: int) -> Users | None:
     """
-        Загружает пользователя из базы данных по заданному идентификатору.
+    Загружает пользователя из базы данных по заданному идентификатору.
 
-        :param user_id: идентификатор пользователя
-        :type user_id: int
-        :return: объект пользователя, если пользователь существует, иначе None
-        :rtype: Users
+    :param user_id: идентификатор пользователя
+    :type user_id: int
+    :return: объект пользователя, если пользователь существует, иначе None
+    :rtype: Users
     """
     return Users.query.get(user_id)
 
 
 with app.app_context():
     db.create_all()
-
