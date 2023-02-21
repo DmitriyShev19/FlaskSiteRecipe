559ea903079181bab346bfa91b763f082e7ef326
dimka2338 <dimka2338@ya.ru>
2023-02-21 18:58:31 +0300

    Подправил файлы с помощью библиотеки black

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'jsdfhjsdhkjfkhsdfhlkjs'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SECRET_KEY"] = "jsdfhjsdhkjfkhsdfhlkjs"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
 manager = LoginManager(app)
 
 toastr = Toastr(app)
-app.config['TOASTR_SHOW_METHOD'] = 'show'
-app.config['TOASTR_TIMEOUT'] = 5000
+app.config["TOASTR_SHOW_METHOD"] = "show"
+app.config["TOASTR_TIMEOUT"] = 5000
 
-UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024
+UPLOAD_FOLDER = "static/uploads"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 3 * 1024 * 1024
 
 toolbar = DebugToolbarExtension(app)
